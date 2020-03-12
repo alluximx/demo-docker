454bcacae4c6b2e573bd21b4f209c83e79cd691c
Adrian Martinez <n.bicalcarata@gmail.com>
2020-03-12 11:55:16 -0600

    Add mancomunado serializer and form fields

--- fecoval/avaluos/serializers.py
+++ fecoval/avaluos/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Cliente, Avaluo, DatosCliente
+from .models import Cliente, Avaluo, DatosCliente, Mancomunado
 
 
 class ClienteSerializer(serializers.ModelSerializer):
@@ -14,19 +14,29 @@ class DatosClienteSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class MancomunadoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Mancomunado
+        fields = '__all__'
+
+
 class AvaluoSerializer(serializers.ModelSerializer):
     cliente = serializers.PrimaryKeyRelatedField(queryset=Cliente.objects.all())
     datos_cliente = DatosClienteSerializer()
+    mancomunado = MancomunadoSerializer()
 
     class Meta:
         model = Avaluo
-        # fields = '__all__'
-        fields = ('cliente', 'datos_cliente')
+        fields = '__all__'
+        # fields = ['cliente', 'datos_cliente', 'mancomunado']
 
     def create(self, validated_data):
         avaluo = Avaluo(cliente=validated_data.get('cliente'))
         datos_cliente = validated_data.get('datos_cliente')
+        mancomunado = validated_data.get('mancomunado')
         datos_cliente_obj = DatosCliente.objects.create(**datos_cliente)
+        mancomunado_obj = Mancomunado.objects.create(**mancomunado)
         avaluo.datos_cliente = datos_cliente_obj
+        avaluo.mancomunado = mancomunado_obj
         avaluo.save()
         return validated_data
