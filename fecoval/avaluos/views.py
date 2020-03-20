94d1943f17a4f4cd5944a878f3995d2cbe0c74c4
Adrian Martinez <n.bicalcarata@gmail.com>
2020-03-20 12:20:44 -0600

    Show active page in navbar

--- fecoval/avaluos/views.py
+++ fecoval/avaluos/views.py
@@ -6,10 +6,16 @@ from django.views import View
 class AvaluoList(LoginRequiredMixin, View):
     def get(self, request):
         template = 'avaluos/list.html'
-        return render(request, template)
+        context = {
+            'avaluos_page': 'active'
+        }
+        return render(request, template, context=context)
 
 
 class AvaluoCreate(LoginRequiredMixin, View):
     def get(self, request):
+        context = {
+            'altas_page': 'active'
+        }
         template = 'avaluos/create.html'
-        return render(request, template)
+        return render(request, template, context=context)
